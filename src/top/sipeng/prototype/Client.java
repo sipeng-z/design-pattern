f9c8c296e4744afdeaf612ef2f9b7653e17dc3a9
spencer <11045897@qq.com>
2020-12-01 16:25:11 +0800

    原型模式

---
+++ src/top/sipeng/prototype/Client.java
@@ -0,0 +1,9 @@
+package top.sipeng.prototype;
+
+/**
+ * @Author Si Peng
+ * @Date 2020/12/1 15:35
+ * @Version 1.0
+ */
+public class Client {
+}
---
+++ src/top/sipeng/prototype/Person.java
@@ -0,0 +1,9 @@
+package top.sipeng.prototype;
+
+/**
+ * @Author Si Peng
+ * @Date 2020/12/1 15:28
+ * @Version 1.0
+ */
+public class Person {
+}
